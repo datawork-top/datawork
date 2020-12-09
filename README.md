24c3bb7eb038c62c40e1897fb878beee3e6b0df9
bigide <peter@bigide.com>
2020-12-09 18:33:45 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,14 @@
+## 数据工作台简介
+
+数据同步Datahub 基于Datax web
+数据可视化FastBI 基于Davinci
+数据集成 基于Kettle
+
+## 在线体验
+
+
+## 演示图
+
+
+
+[LICENSE](LICENSE)
---
+++ datawork-portal-ui/src/components/Datawork/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataworkDoc',
+  data() {
+    return {
+      url: 'http://datawork.top/doc'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
